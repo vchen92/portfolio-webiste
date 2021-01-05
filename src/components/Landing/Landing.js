4ac565533e19f5311a2a1fabc6e7a33c4ee038b8
Vincent Chen <vchen92@users.noreply.github.com>
2021-01-05 00:22:06 -0800

    css

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -6,19 +6,11 @@ function Landing() {
 	return (
 		<div className="landing section">
 			<div className="landing__container">
-				<div>
-					<h1 className="landing__hi roboto-font teal">
-						Hi, my name is
-					</h1>
-				</div>
-				<div>
-					<h2 className="landing__name">Vincent Chen.</h2>
-				</div>
-				<div>
-					<h3 className="landing__subheader">
-						I build things for the web.
-					</h3>
-				</div>
+				<h1 className="landing__hi roboto-font teal">Hi, my name is</h1>
+				<h2 className="landing__name">Vincent Chen.</h2>
+				<h3 className="landing__subheader">
+					I build things for the web.
+				</h3>
 				<div className="landing__about">
 					I'm a software engineer based in Vancouver, BC specializing
 					in building (and occasionally designing) exceptional
