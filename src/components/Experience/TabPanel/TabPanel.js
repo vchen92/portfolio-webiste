91b68fc4dd78ccdb56f46184944fd0b40c0493cd
Vincent Chen <vchen92@users.noreply.github.com>
2021-01-06 16:41:30 -0800

    tab panels

---
+++ src/components/Experience/TabPanel/TabPanel.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './TabPanel.css';
+import HyperLink from './../../HyperLink/HyperLink';
+
+function TabPanel({ position, company, url, startDate, endDate, items }) {
+	return (
+		<div className="tabPanel">
+			<p className="tabPanel__heading">
+				<span className="tabPanel__position">{position}</span>{' '}
+				<span className="tabPanel__company">
+					@ <HyperLink url={url}>{company}</HyperLink>
+				</span>
+			</p>
+			<p className="tabPanel__date">
+				{startDate} - {endDate}
+			</p>
+			<ul className="tabPanel__list">
+				{items.map(item => (
+					<li>{item}</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default TabPanel;
