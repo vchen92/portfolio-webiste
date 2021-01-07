972c1852795b80755b6b6c5d4886daecb515c610
Vincent Chen <vchen92@users.noreply.github.com>
2021-01-07 00:49:57 -0800

    project card skeleton

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import HyperLink from './../UI/HyperLink/HyperLink';
+import './ProjectCard.css';
 
 function ProjectCard({ title, description, technologies, url, githubUrl }) {
 	return (
 		<div className="project">
 			<p className="project__featured">Featured Project</p>
 			<h2 className="project__title">{title}</h2>
-			<p className="project__description">{description}</p>
+			<div className="project__description">{description}</div>
 			<div className="project__technologies">
 				{technologies.map(tech => (
 					<span>{tech}</span>
 				))}
 			</div>
 			<div className="project__links">
-				<HyperLink url={url}>{url}</HyperLink>
-				<HyperLink url={githubUrl}>{githubUrl}</HyperLink>
+				<span>
+					<HyperLink url={url}>Website</HyperLink>
+				</span>
+				<span>
+					<HyperLink url={githubUrl}>GitHub</HyperLink>
+				</span>
 			</div>
 		</div>
 	);
