a919b8505ad8926738f0849bc259675e9d5789c9
Vincent Chen <vchen92@users.noreply.github.com>
2021-01-09 03:11:01 -0800

    unresponsive projectCard

--- src/components/ProjectCard/ProjectCard.js
+++ src/components/ProjectCard/ProjectCard.js
@@ -4,22 +4,33 @@ import './ProjectCard.css';
 
 function ProjectCard({ title, description, technologies, url, githubUrl }) {
 	return (
-		<div className="project">
-			<p className="project__featured">Featured Project</p>
-			<h2 className="project__title">{title}</h2>
-			<div className="project__description">{description}</div>
-			<div className="project__technologies">
-				{technologies.map(tech => (
-					<span>{tech}</span>
-				))}
+		<div className="projectCard">
+			<div className="projectCard__image-container">
+				<a href="www.google.com">
+					<img
+						className="projectCard__image"
+						src="https://www.scnsoft.com/blog-pictures/web-apps/web-application-vs-website-01.png"
+					/>
+				</a>
 			</div>
-			<div className="project__links">
-				<span>
-					<HyperLink url={url}>Website</HyperLink>
-				</span>
-				<span>
-					<HyperLink url={githubUrl}>GitHub</HyperLink>
-				</span>
+
+			<div className="projectCard__content">
+				<p className="projectCard__featured">Featured Project</p>
+				<h2 className="projectCard__title">{title}</h2>
+				<div className="projectCard__description">{description}</div>
+				<div className="projectCard__technologies">
+					{technologies.map(tech => (
+						<span>{tech}</span>
+					))}
+				</div>
+				<div className="projectCard__links">
+					<span>
+						<HyperLink url={url}>Website</HyperLink>
+					</span>
+					<span>
+						<HyperLink url={githubUrl}>GitHub</HyperLink>
+					</span>
+				</div>
 			</div>
 		</div>
 	);
